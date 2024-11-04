3a723ae554861d9f28ae0a7e3d76bf4df91f4410
bazil <vasilij.safronov@yahoo.com>
2024-11-04 12:18:07 +0300

    add config prettier

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
-} from "react-router-dom";
-import Dashboard from "@/pages/dashboard";
-import Accounts from "@/pages/accounts";
-import Wallet from "@/pages/wallet";
-import Welcome from "@/pages/welcome";
-import Analytics from "@/pages/analytics";
-import Settings from "@/pages/settings";
-import Security from "@/pages/security";
-import Signup from "@/pages/signup";
-import Help from "@/pages/help";
-import Layout from "@/widgets/Layout/ui/Layout";
-import { useAuth } from "@/shared/providers/AuthContext";
+} from 'react-router-dom';
+import Dashboard from '@/pages/dashboard';
+import Accounts from '@/pages/accounts';
+import Wallet from '@/pages/wallet';
+import Welcome from '@/pages/welcome';
+import Analytics from '@/pages/analytics';
+import Settings from '@/pages/settings';
+import Security from '@/pages/security';
+import Signup from '@/pages/signup';
+import Help from '@/pages/help';
+import Layout from '@/widgets/Layout/ui/Layout';
+import { useAuth } from '@/shared/providers/AuthContext';
 
 function App() {
   const { user } = useAuth();
@@ -24,27 +24,25 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/*  path="/signup"*/}
-        {/*  element={*/}
-        {/*    !isAuthenticated ? <Signup /> : <Navigate replace to="/dashboard" />*/}
-        {/*  }*/}
-        {/*/>*/}
-
-        {/*/!* Все защищенные маршруты должны быть вложены в Layout *!/*/}
-        {/*<Route*/}
-        {/*  element={*/}
-        {/*    isAuthenticated ? <Layout /> : <Navigate replace to="/signup" />*/}
-        {/*  }*/}
-        {/*>*/}
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="accounts" element={<Accounts />} />
-        <Route path="wallet" element={<Wallet />} />
-        <Route path="analytics" element={<Analytics />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="security" element={<Security />} />
-        <Route path="help" element={<Help />} />
-
-        {/* Незащищенный маршрут */}
+        <Route
+          path="/signup"
+          element={
+            !isAuthenticated ? <Signup /> : <Navigate replace to="/dashboard" />
+          }
+        />
+        <Route
+          element={
+            isAuthenticated ? <Layout /> : <Navigate replace to="/signup" />
+          }
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="accounts" element={<Accounts />} />
+          <Route path="wallet" element={<Wallet />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="security" element={<Security />} />
+          <Route path="help" element={<Help />} />
+        </Route>
         <Route path="/" element={<Welcome />} />
       </Routes>
     </Router>
